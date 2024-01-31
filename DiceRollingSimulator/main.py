5d8eac0a72ebb92a146f5153b48fa87075dac769
Matteo <matteo.langone@gmail.com>
2024-02-01 00:19:54 +0100

    Added different type of Dice and enhanced the output print

--- DiceRollingSimulator/main.py
+++ DiceRollingSimulator/main.py
@@ -12,31 +12,86 @@ user. If the user chooses to exit, the program will simply exit. """
 from random import seed
 from random import randint
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 def main():
     InitRollingSimulator()
+
+def RollD4():
+    value = randint(1, 4)
+    print("")
+    print(color.BOLD + "The Dice result is " +color.RED + f"{value}" + color.END)
 
 def RollD6():
     value = randint(1, 6)
     print("")
-    print(f"Ho tirato il dado ed è uscito {value}")
+    print(color.BOLD + "The Dice result is " +color.RED + f"{value}" + color.END)
+
+def RollD8():
+    value = randint(1, 8)
+    print("")
+    print(color.BOLD + "The Dice result is " +color.RED + f"{value}" + color.END)
+
+def RollD10():
+    value = randint(1, 10)
+    print("")
+    print(color.BOLD + "The Dice result is " +color.RED + f"{value}" + color.END)
+
+def RollD12():
+    value = randint(1, 12)
+    print("")
+    print(color.BOLD + "The Dice result is " +color.RED + f"{value}" + color.END)
+
+def RollD20():
+    value = randint(1, 20)
+    print("")
+    print(color.BOLD + "The Dice result is " +color.RED + f"{value}" + color.END)
 
 def InitRollingSimulator():
     print("")
-    print("Choose one option below:")
-    print("1. Roll a D6")
-    print("6. Exit")
+    print(color.BOLD + color.GREEN + "Choose one option below:" + color.END)
+    print("1. Roll a D4     4. Roll a D10")
+    print("2. Roll a D6     5. Roll a D12")
+    print("3. Roll a D8     6. Roll a D20")
+    print("0. Exit")
     c = int(input("Insert the number of action you want to do: "))
 
     match c:
         case 1:
+            RollD4()
+            InitRollingSimulator()
+        case 2:
             RollD6()
             InitRollingSimulator()
+        case 3:
+            RollD8()
+            InitRollingSimulator()
+        case 4:
+            RollD10()
+            InitRollingSimulator()
+        case 5:
+            RollD12()
+            InitRollingSimulator()
         case 6:
+            RollD20()
+            InitRollingSimulator()
+        case 0:
             print("")
-            print("Exiting")
+            print(color.BOLD + color.GREEN + "Exiting" + color.END)
+            print("")
         case _:
             print("")
-            print("Please choose a value from the list!")
+            print(color.BOLD + "Please choose a value from the list!" + color.END)
             InitRollingSimulator()
 
 if __name__ == "__main__":
