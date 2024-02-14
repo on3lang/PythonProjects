6f4d116c3f23d6d620fc30148c72b7cdd2b988b0
Matteo <matteo.langone@enel.com>
2024-02-14 12:23:03 +0100

    added comment to main file of Value Converter

--- Value Converter/main.py
+++ Value Converter/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+#Color class to make the output more readable
+
 class color:
    PURPLE = '\033[95m'
    CYAN = '\033[96m'
@@ -12,10 +14,12 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
+#Main function to start the program
 def main():
     print(color.BOLD + color.BLUE + "Welcome to the Value Converter" + color.END)
     startConverter()
 
+#Function to start the converter
 def startConverter():
     print("")
     print("1. C to F")
@@ -56,25 +60,31 @@ def startConverter():
             print(color.BOLD + color.GREEN + "Please choose a value from the list!" + color.END)
             startConverter()
 
+#Functions to convert the values from Grados Celsius to Fahrenheit   
 def CtoF(c):
     f = (c * 9/5) + 32
     print(color.GREEN + f"{c}C is {f}F" + color.END)
 
+#Functions to convert the values from Fahrenheit to Grados Celsius
 def FtoC(f):
     c = (f - 32) * 5/9
     print(color.GREEN + f"{f}F is {c}C" + color.END)
 
+#Functions to convert the values from EUR to USD
 def EURtoUSD(e):
     u = e * 1.13
     print(color.GREEN + f"{e}EUR is {u}USD" + color.END)
 
+#Functions to convert the values from USD to EUR
 def USDtoEUR(u):
     e = u / 1.13
     print(color.GREEN + f"{u}USD is {e}EUR" + color.END)
 
+#Function to calculate the circumference of a circle
 def calcCircunference(r):
     c = r * np.pi
     print(color.GREEN + f"Given the radius {r}, the circuference is {c}" + color.END)
 
+#Start the program
 if __name__ == "__main__":
     main()
